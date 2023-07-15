195a53c98e8a23e85f879891f94172f24ef9591e
chiangjou <tiffanyj.chiang@gmail.com>
2023-07-15 19:41:33 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const exphbs = require("express-handlebars")
 const bodyParser = require("body-parser")
 const methodOverride = require("method-override") 
 const session = require("express-session")
+const flash = require("connect-flash")
 
 // 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== "production") {
@@ -31,6 +32,15 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride("_method"))
 
 usePassport(app)
+
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  res.locals.success_msg = req.flash("success_msg")
+  res.locals.warning_msg = req.flash("warning_msg")
+  next()
+})
 
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
