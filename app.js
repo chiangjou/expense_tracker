71a83a6d715947a26944ab36877ca597826e4e35
chiangjou <tiffanyj.chiang@gmail.com>
2023-07-15 19:15:55 +0800

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -4,15 +4,14 @@ const bodyParser = require("body-parser")
 const methodOverride = require("method-override") 
 const session = require("express-session")
 
-
-const Record = require("./models/record")
-
 // 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config()
 }
 
 const routes = require("./routes")
+
+const usePassport = require("./config/passport")
 require("./config/mongoose")
 
 const app = express()
@@ -30,6 +29,8 @@ app.use(session({
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(methodOverride("_method"))
+
+usePassport(app)
 
 app.use(routes)
 
