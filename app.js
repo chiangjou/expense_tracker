64d3b838fc280269e5e20c289287c4f95158b261
chiangjou <tiffanyj.chiang@gmail.com>
2023-07-15 18:19:51 +0800

    feat: add delete record function

--- app.js
+++ app.js
@@ -78,6 +78,15 @@ app.post("/records/:id/edit", (req, res) => {
     .catch(error => console.log(error))
 })
 
+// Delete
+app.post("/records/:id/delete", (req, res) => {
+  const id = req.params.id
+  return Record.findById(id)
+    .then(record => record.remove())
+    .then(() => res.redirect("/"))
+    .catch(error => console.log(error))
+})
+
 app.listen(PORT, () => {
   console.log(`Express is listening on http://localhost:${PORT}`)
 })
