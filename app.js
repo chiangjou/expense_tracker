9b47f338ef20f8f1f58cfc8ad1256e9da9f8b1f5
chiangjou <tiffanyj.chiang@gmail.com>
2023-07-15 18:46:16 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require("express")
-const mongoose = require("mongoose")
 const exphbs = require("express-handlebars")
 const bodyParser = require("body-parser")
 const methodOverride = require("method-override") 
@@ -12,21 +11,10 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 const routes = require("./routes")
+require("./config/mongoose")
+
 const app = express()
 const PORT = process.env.PORT
-
-mongoose.connect(process.env.MONGODB_URI, { 
-  useNewUrlParser: true, 
-  useUnifiedTopology: true })
-
-// 取得資料庫連線狀態
-const db = mongoose.connection
-db.on("error", () => {
-  console.log("mongodb error!")
-})
-db.once("open", () => {
-  console.log("mongodb connected!")
-})
 
 app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }))
 app.set("view engine", "hbs")
