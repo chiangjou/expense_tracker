3c5c64c501015b5b82a168c6c62f40eb2ca51926
Chiang Jou <tiffanyj.chiang@gmail.com>
2023-07-17 11:21:28 +0800

    feat: delete date function

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -3,20 +3,6 @@ const router = express.Router()
 const Record = require("../../models/record")
 const Category = require("../../models/category")
 
-// 函式庫
-const utilities = {
-  ConvertToSlashDate(dashDate) {
-    return new Date(dashDate).toLocaleDateString("zh-TW")
-  },
-  ConvertToDashDate(slashDate) {
-    // 1. 取出日期，並將年月日以字串格式分開存入陣列，並以padStart函數補0
-    const date = new Date(slashDate)
-    const dateArray = [date.getFullYear().toString(10), (date.getMonth() + 1).toString(10).padStart(2, "0"), date.getDate().toString(10).padStart(2, "0")]
-    // 2. 重組成字串
-    return dateArray.join("-")
-  }
-}
-
 // Create
 router.get("/new", (req, res) => {
   // 找出 Category 的資料，結果存到 categories
@@ -31,9 +17,6 @@ router.post("/", (req, res) => {
   const userId = req.user._id
   const { name, date, categoryName, amount } = req.body
   
-  // 將日期從 YYYY-MM-DD 轉成 YYYY/MM/DD
-  let slashDate = utilities.ConvertToSlashDate(date)
-
   Category.findOne({ name: categoryName })
     .lean()
     .then(category => {
@@ -42,7 +25,7 @@ router.post("/", (req, res) => {
 
       Record.create({
         name,
-        date: slashDate,
+        date,
         amount,
         userId,
         categoryId
@@ -64,15 +47,12 @@ router.get("/:id", (req, res) => {
         .populate("categoryId")
         .lean()
         .then(record => {
-          // 將日期從 YYYY-MM-DD
-          dashDate = utilities.ConvertToDashDate(record.date)
-
           categories.map((category, index) => {
             if (category.name === record.categoryId.name) {
               categories[index]["isChoosed"] = true
             }
           })
-          res.render("edit", { categories, record, dashDate })
+          res.render("edit", { categories, record })
         })
         .catch(error => console.log(error))
     })
@@ -83,14 +63,13 @@ router.put("/:id", (req, res) => {
   const userId = req.user._id
   const _id = req.params.id
   const { name, date, categoryName, amount } = req.body
-  let slashDate = utilities.ConvertToSlashDate(date)
 
   Category.findOne({ name: categoryName })
     .then(category => {
       Record.findOne({ _id, userId })
         .then(record => {
           record.name = name
-          record.date = slashDate
+          record.date = date
           record.amount = amount
           record.categoryId = category._id
 
