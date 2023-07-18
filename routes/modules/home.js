2b99ca6ca0462d23338d8a146fb52637a399ed8d
chiangjou <tiffanyj.chiang@gmail.com>
2023-07-18 23:48:29 +0800

    feat: modify filter

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -2,7 +2,6 @@ const express = require("express")
 const router = express.Router()
 const Record = require("../../models/record")
 const Category = require("../../models/category")
-const category = require("../../models/category")
 
 router.get("/", (req, res) => {
   const userId = req.user._id
@@ -17,8 +16,7 @@ router.get("/", (req, res) => {
         .then(records => {
           let totalAmount = 0
 
-          records.map((record) => {
-            // 計算花費總額
+          records.forEach(record => {
             totalAmount += record.amount
           })
           res.render("index", { categories, records, totalAmount })
@@ -26,31 +24,38 @@ router.get("/", (req, res) => {
     })
 })
 
-
 router.post("/", (req, res) => {
   const userId = req.user._id
-  const categoryID = req.body.categoryId
+  const categoryName = req.body.categoryId
 
-  Record.find({ userId, categoryId: categoryID })
-    .populate("categoryId")
+  if (!categoryName) {
+    return res.redirect("/")
+  }
+
+  Record.find({ userId })
+    .populate({
+      path: "categoryId",
+      match: { name: categoryName },   // 使用類別名稱來查詢相應的記錄
+      select: "name",   // 只選擇類別的名稱，避免其他數據的回傳
+    })
     .lean()
     .sort({ date: "desc" })
-    .then((records) => {
+    .then(records => {
       let totalAmount = 0;
+      records = records.filter(record => record.categoryId !== null);   // 過濾掉未匹配到類別的記錄
+      records.forEach(record => {
+        totalAmount += record.amount;
+        record.categoryId["isChoosed"] = true;
+      });
 
-      records.forEach((record) => {
-        totalAmount += record.amount
-        
-        if (record.categoryId._id.toString() === categoryID) {
-          record.categoryId["isChoosed"] = true
-        } else {
-          record.categoryId["isChoosed"] = false
-        }
-      })
-
-      res.render("index", { records, totalAmount })
+      Category.find()
+        .lean()
+        .then(categories => {
+          res.render("index", { categories, records, totalAmount })
+        })
     })
-    .catch((error) => {console.error(error)})
+    .catch(error => {console.error(error)})
 });
 
-module.exports = router
+
+      module.exports = router
